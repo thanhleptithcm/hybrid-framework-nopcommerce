343299dfc567d7e1e3b7a4e5a8584cb913cb3180
Cafeland <cafeland@Cafelands-Mac-mini.local>
2024-06-22 09:41:26 +0700

    update code

---
+++ actions/pageObjects/RegisterPageObject.java
@@ -0,0 +1,19 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+import pageUIs.RegisterPageUI;
+
+public class RegisterPageObject extends BasePage {
+	private WebDriver driver;
+	
+	public RegisterPageObject(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void clickToRegisterButton() {
+		waitForElementClickable(driver, RegisterPageUI.REGISTER_BUTTON);
+		clickToElement(driver, RegisterPageUI.REGISTER_BUTTON);
+	}
+}
---
+++ interfaces/pageUIs/RegisterPageUI.java
@@ -0,0 +1,5 @@
+package pageUIs;
+
+public class RegisterPageUI {
+	public static final String REGISTER_BUTTON = "//button[@id='register-button']";
+}
